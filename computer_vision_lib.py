6a96cc1762598020b5f25ba620b3644c9080478e
AlexanderPilz <51150328+xcomagent95@users.noreply.github.com>
2022-01-03 14:17:53 +0100

    some refinements

--- computer_vision_lib.py
+++ computer_vision_lib.py
@@ -44,292 +44,18 @@ class position:
 def arrayToImage(imageArray):
     image = Image.fromarray(imageArray, mode='RGB')
     image.show()
+    
+def imageToArray(imagePath):
+    image = Image.open(imagePath)
+    imageArray = np.asarray(image).copy()
+    
+    return imageArray
 
-def printHistogramm(imagePath):
-    image = Image.open(imagePath)
+def sobelHorizontalFilter(imageArray):
     #Image Info
-    print(image.size)
-    print(image.info)
-    width = image.width
-    height = image.height
-    imageArray = np.asarray(image)
-    
-    histogramArray = []
-    for i in range(height):
-        for j in range(width):
-           histogramArray.append(imageArray[i][j][0])
-           #Plot original Histogramm
-    plt.hist(histogramArray, bins=255)
-    
-def brightenImage(imagePath, value):    
-    image = Image.open(imagePath)
-    #Image Info
-    print(image.size)
-    print(image.info)
-    width = image.width
-    height = image.height
-    imageArray = np.asarray(image).copy()
-    histogramArray = []
-    for i in range(height):
-        for j in range(width):
-           if(imageArray[i][j][0] + value > 255):
-               imageArray[i][j][0] = 255
-               imageArray[i][j][1] = 255
-               imageArray[i][j][2] = 255
-           else:
-               imageArray[i][j][0] = imageArray[i][j][0] + value
-               imageArray[i][j][1] = imageArray[i][j][1] + value
-               imageArray[i][j][2] = imageArray[i][j][2] + value
-               
-           histogramArray.append(imageArray[i][j][0])
-           #Plot original Histogramm
-    plt.hist(histogramArray, bins=255)
-    brightendImage = Image.fromarray(imageArray, mode='RGB')
-    brightendImage.show()
-    
-def darkenImage(imagePath, value):    
-    image = Image.open(imagePath)
-    #Image Info
-    print(image.size)
-    print(image.info)
-    width = image.width
-    height = image.height
-    imageArray = np.asarray(image).copy()
-    histogramArray = []
-    for i in range(height):
-        for j in range(width):
-           if(imageArray[i][j][0] - value < 0):
-               imageArray[i][j][0] = 0
-               imageArray[i][j][1] = 0
-               imageArray[i][j][2] = 0
-           else:
-               imageArray[i][j][0] = imageArray[i][j][0] - value
-               imageArray[i][j][1] = imageArray[i][j][1] - value
-               imageArray[i][j][2] = imageArray[i][j][2] - value
-               
-           histogramArray.append(imageArray[i][j][0])
-           #Plot original Histogramm
-    plt.hist(histogramArray, bins=255)
-    darkenedImage = Image.fromarray(imageArray, mode='RGB')
-    darkenedImage.show()
-    
-def negativeImage(imagePath):    
-    image = Image.open(imagePath)
-    #Image Info
-    print(image.size)
-    print(image.info)
-    width = image.width
-    height = image.height
-    imageArray = np.asarray(image).copy()
-    histogramArray = []
-    for i in range(height):
-        for j in range(width):
-               imageArray[i][j][0] = 255-imageArray[i][j][0] 
-               imageArray[i][j][1] = 255-imageArray[i][j][1] 
-               imageArray[i][j][2] = 255-imageArray[i][j][2]
-               histogramArray.append(imageArray[i][j][0])
-           #Plot original Histogramm
-    plt.hist(histogramArray, bins=255)
-    negativeImage = Image.fromarray(imageArray, mode='RGB')
-    negativeImage.show()
-    
-def nonLinearLogTransformedImage(imagePath):    
-    image = Image.open(imagePath)
-    #Image Info
-    print(image.size)
-    print(image.info)
-    width = image.width
-    height = image.height
-    imageArray = np.asarray(image).copy()
-    histogramArray = []
-    for i in range(height):
-        for j in range(width):
-               imageArray[i][j][0] = 255*(math.log2(1+imageArray[i][j][0])/math.log2(256))
-               imageArray[i][j][1] = 255*(math.log2(1+imageArray[i][j][1])/math.log2(256))
-               imageArray[i][j][2] = 255*(math.log2(1+imageArray[i][j][2])/math.log2(256))
-               histogramArray.append(imageArray[i][j][0])
-           #Plot original Histogramm
-    plt.hist(histogramArray, bins=255)
-    nonLinearLogTransformedImage = Image.fromarray(imageArray, mode='RGB')
-    nonLinearLogTransformedImage.show()
-
-def exponentiallyTransformedImage(imagePath, exponent):    
-    image = Image.open(imagePath)
-    #Image Info
-    print(image.size)
-    print(image.info)
-    width = image.width
-    height = image.height
-    imageArray = np.asarray(image).copy()
-    histogramArray = []
-    for i in range(height):
-        for j in range(width):
-               imageArray[i][j][0] = 255*(imageArray[i][j][0]/255)**exponent
-               imageArray[i][j][1] = 255*(imageArray[i][j][1]/255)**exponent
-               imageArray[i][j][2] = 255*(imageArray[i][j][2]/255)**exponent
-               histogramArray.append(imageArray[i][j][0])
-    #Plot original Histogramm
-    plt.hist(histogramArray, bins=255)
-    exponentiallyTransformedImage = Image.fromarray(imageArray, mode='RGB')
-    exponentiallyTransformedImage.show()
-
-def meanFilter(imagePath):
-    image = Image.open(imagePath)
-    #Image Info
-    print(image.size)
-    print(image.info)
-    height = image.height
-    width = image.width
-    imageArray = np.asarray(image).copy()
-    #define mean kernel
-    kernel = [[1,1,1],[1,1,1],[1,1,1]]
-    
-    #expand image array
-    meanFilteredImageArray = imageArray.copy()
-    for i in range(1, height-1): #rows
-        for j in range(1, width-1): #columns
-            #apply kernel
-            values = []
-            for k in range(-1, 2):
-                for h in range(-1, 2):
-                    values.append(imageArray[i+k][j+h])      
-            value = 0
-            counter = 0
-            for ki in range(0, len(kernel)):
-                for kj in range(0, len(kernel[0])):
-                    value = value + values[counter][0] * kernel[ki][kj]
-                    counter = counter + 1
-            meanFilteredImageArray[i][j][0] = int(value/9) 
-            meanFilteredImageArray[i][j][1] = int(value/9) 
-            meanFilteredImageArray[i][j][2] = int(value/9) 
-    
-    return meanFilteredImageArray
-
-def medianFilter(imagePath):
-    image = Image.open(imagePath)
-    #Image Info
-    print(image.size)
-    print(image.info)
-    height = image.height
-    width = image.width
-    imageArray = np.asarray(image).copy()
-    
-    #expand image array
-    medianFilteredImageArray = imageArray.copy()
-    for i in range(1, height-1): #rows
-        for j in range(1, width-1): #columns
-            values = []
-            for k in range(-1, 2):
-                for h in range(-1, 2):
-                    values.append(imageArray[i+k][j+h])      
-            medianFilteredImageArray[i][j][0] = np.median(values)
-            medianFilteredImageArray[i][j][1] = np.median(values)
-            medianFilteredImageArray[i][j][2] = np.median(values)  
-
-    return medianFilteredImageArray            
-                    
-def gaussFilter(imagePath):
-    image = Image.open(imagePath)
-    #Image Info
-    print(image.size)
-    print(image.info)
-    height = image.height
-    width = image.width
-    imageArray = np.asarray(image).copy()
-    #define gaussian kernel
-    kernel = [[1,2,1],[2,4,2],[1,2,1]]
-
-    #expand image array
-    gaussFilteredImageArray = imageArray.copy()
-    for i in range(1, height-1): #rows
-        for j in range(1, width-1): #columns
-            #apply kernel
-            values = []
-            for k in range(-1, 2):
-                for h in range(-1, 2):
-                    values.append(imageArray[i+k][j+h])      
-            value = 0
-            counter = 0
-            for ki in range(0, len(kernel)):
-                for kj in range(0, len(kernel[0])):
-                    value = value + values[counter][0] * kernel[ki][kj]
-                    counter = counter + 1
-            gaussFilteredImageArray[i][j][0] = int(value/16) 
-            gaussFilteredImageArray[i][j][1] = int(value/16) 
-            gaussFilteredImageArray[i][j][2] = int(value/16) 
-
-    return gaussFilteredImageArray
-
-def prewittHorizontalFilter(imagePath):
-    image = Image.open(imagePath)
-    #Image Info
-    print(image.size)
-    print(image.info)
-    height = image.height
-    width = image.width
-    imageArray = np.asarray(image).copy()
-    
-    prewittHorizontalFilteredImageArray = imageArray.copy()
-    
-    kernel = [[1,1,1],[0,0,0],[-1,-1,-1]]
-    
-    for i in range (1, height-1):
-        for j in range (1, width-1):
-            values = []
-            for k in range(-1, 2):
-                for h in range(-1, 2):
-                    values.append(imageArray[i+k][j+h][0]) 
-            value = 0
-            counter = 0
-            for ki in range(0, len(kernel)):
-                for kj in range(0, len(kernel[0])):
-                    value = value + values[counter] * kernel[ki][kj]
-                    counter += 1
-            prewittHorizontalFilteredImageArray[i][j][0] = abs(value)
-            prewittHorizontalFilteredImageArray[i][j][1] = abs(value)
-            prewittHorizontalFilteredImageArray[i][j][2] = abs(value) 
-    
-    return prewittHorizontalFilteredImageArray
-
-def prewittVerticalFilter(imagePath):
-    image = Image.open(imagePath)
-    #Image Info
-    print(image.size)
-    print(image.info)
-    height = image.height
-    width = image.width
-    imageArray = np.asarray(image).copy()
-    
-    prewittVerticalFilteredImageArray = imageArray.copy()
-    
-    kernel = [[-1,0,1],[-1,0,1],[-1,0,1]]
-    
-    for i in range (1, height-1):
-        for j in range (1, width-1):
-            values = []
-            for k in range(-1, 2):
-                for h in range(-1, 2):
-                    values.append(imageArray[i+k][j+h][0]) 
-            value = 0
-            counter = 0
-            for ki in range(0, len(kernel)):
-                for kj in range(0, len(kernel[0])):
-                    value = value + values[counter] * kernel[ki][kj]
-                    counter += 1
-            prewittVerticalFilteredImageArray[i][j][0] = abs(value)
-            prewittVerticalFilteredImageArray[i][j][1] = abs(value)
-            prewittVerticalFilteredImageArray[i][j][2] = abs(value) 
-    
-    return prewittVerticalFilteredImageArray
-    
-def sobelHorizontalFilter(imagePath):
-    image = Image.open(imagePath)
-    #Image Info
-    print(image.size)
-    print(image.info)
-    height = image.height
-    width = image.width
-    imageArray = np.asarray(image).copy()
+    height = len(imageArray)
+    width = len(imageArray[0])
+    imageArray = imageArray
     
     sobelHorizontalFilteredImageArray = imageArray.copy()
     
@@ -377,14 +103,11 @@ def sobelHorizontalFilter(imagePath):
         
     return sobelHorizontalFilteredImageArray
         
-def sobelVerticalFilter(imagePath):
-    image = Image.open(imagePath)
+def sobelVerticalFilter(imageArray):
     #Image Info
-    print(image.size)
-    print(image.info)
-    height = image.height
-    width = image.width
-    imageArray = np.asarray(image).copy()
+    height = len(imageArray)
+    width = len(imageArray[0])
+    imageArray = imageArray
     
     sobelVerticalFilteredImageArray = imageArray.copy()
     
@@ -432,35 +155,13 @@ def sobelVerticalFilter(imagePath):
 
     return sobelVerticalFilteredImageArray
 
-def fullPrewittFilter(imagePath):
-    image = Image.open(imagePath)
-    imageArray = np.asarray(image).copy()
+def fullSobelFilter(imageArray):
     
-    height = image.height
-    width = image.width
+    height = len(imageArray)
+    width = len(imageArray[0])
     
-    horizontalPrewittArray = prewittHorizontalFilter(imagePath)
-    verticalPrewittArray = prewittVerticalFilter(imagePath)
-    
-    prewittFilteredImageArray = imageArray.copy()
-    
-    for i in range (1, height-1):
-        for j in range (1, width-1):
-            prewittFilteredImageArray[i][j][0] = horizontalPrewittArray[i][j][0] + verticalPrewittArray[i][j][0]
-            prewittFilteredImageArray[i][j][1] = horizontalPrewittArray[i][j][1] + verticalPrewittArray[i][j][1]
-            prewittFilteredImageArray[i][j][2] = horizontalPrewittArray[i][j][2] + verticalPrewittArray[i][j][2]
-
-    return prewittFilteredImageArray
-    
-def fullSobelFilter(imagePath):
-    image = Image.open(imagePath)
-    imageArray = np.asarray(image).copy()
-    
-    height = image.height
-    width = image.width
-    
-    horizontalSobelArray = sobelHorizontalFilter(imagePath)
-    verticalSobelArray = sobelVerticalFilter(imagePath)
+    horizontalSobelArray = sobelHorizontalFilter(imageArray)
+    verticalSobelArray = sobelVerticalFilter(imageArray)
     
     sobelFilteredImageArray = imageArray.copy()
     
@@ -489,7 +190,6 @@ def minimalSeam(energyMap):
         index = height-i
         values = []
         p = s.getLastPosition()
-        #p.printPosition()
         if(p.y == 0):
            p1 = position(index, p.y, energyMap[index][p.y]) 
            p2 = position(index, p.y+1, energyMap[index][p.y+1]) 
@@ -520,13 +220,12 @@ def minimalSeam(energyMap):
     return s
     
 
-def findVerticalSeam(imagePath):
-    image = Image.open(imagePath)
+def findVerticalSeam(imageArray):
     
-    height = image.height
-    width = image.width
+    height = len(imageArray)
+    width = len(imageArray[0])
     
-    sobelFilteredImageArray = fullSobelFilter(imagePath)
+    sobelFilteredImageArray = fullSobelFilter(imageArray)
     energyMap = []
     
     firstRow = []
@@ -534,7 +233,7 @@ def findVerticalSeam(imagePath):
        firstRow.append(sobelFilteredImageArray[0][i][0])
     energyMap.append(firstRow)
     
-    for i in range (1, height):
+    for i in range (1, height-1):
         row = []
         for j in range (0, width):
                 if j == 0:
@@ -557,38 +256,28 @@ def findVerticalSeam(imagePath):
     
     return seam
 
-def removeVerticalSeam(imagePath, seam):
+def removeVerticalSeam(imageArray, seam):
     
-    image = Image.open(imagePath)
-    
-    height = image.height
-    width = image.width
-    
-    imageArray = np.asarray(image).copy()    
-    
-    #imageArray = np.delete(imageArray, 0, 0)
-    #imageArray = np.delete(imageArray, height-2, 0)
-    #imageArray = np.delete(imageArray, 0, 1)
-    #imageArray = np.delete(imageArray, width-2, 1)
-    print(type(imageArray))
+    height = len(imageArray)
+    width = len(imageArray[0])
+
     newImageArray = imageArray.copy()
-    newImageArray = np.delete(newImageArray, width-3, 1)
-    print(len(newImageArray))
-    print(len(newImageArray[0]))
-    for i in range (0, len(seam.positions)):
-        row = imageArray[i-1]
+    newImageArray = np.delete(newImageArray, width-1, 1)
+    for i in range (0, height-1):
+        row = imageArray[i]
         newRow = np.delete(row, seam.positions[i].y, 0)
         newImageArray[i] = newRow
     
-    im = Image.fromarray(newImageArray)
-    im.save("seamCarvedImage.jpeg")
+    return newImageArray
 
 def removeVerticalSeams(imagePath, numberOfSeams):
-    
-    for i in range(0, numberOfSeams):
-        s = findVerticalSeam(imagePath)
-        removeVerticalSeam(imagePath, s)
-        imagePath = "seamCarvedImage.jpeg"
+    imageArray = imageToArray(imagePath)
+    for i in range(0, numberOfSeams-1):
+        s = findVerticalSeam(imageArray)
+        imageArray = removeVerticalSeam(imageArray, s)
+        
+    result = Image.fromarray(imageArray)
+    result.save("test.jpeg")
     
     
     
--- seam_carving.py
+++ seam_carving.py
@@ -1,9 +1,6 @@
 import computer_vision_lib as cvl
 
 #-----------------------------------------------------------------------------
-#cvl.arrayToImage(cvl.fullSobelFilter("images/delphin.jpg"))
-#cvl.removeVerticalSeam("images/mann_auf_pferd.jpg", cvl.findVerticalSeam("images/mann_auf_pferd.jpg"))
-
-cvl.removeVerticalSeams("images/mann_auf_pferd.jpg", 1)
-
-#cvl.arrayToImage(cvl.sobelHorizontalFilter("images/mann_auf_pferd.jpg"))
+#cvl.removeVerticalSeams("images/mann_auf_pferd.jpg", 5)
+#cvl.arrayToImage(cvl.fullSobelFilter("images/lena.png"))
+cvl.removeVerticalSeams("images/delphin.jpg", 2)
